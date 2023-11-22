8bc3425d6984a4161ef7a7be9903ec95a0f4f416
Purvesh Rathod <purvesh62@gmail.com>
2023-11-22 16:56:25 -0400

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import ChatBot from "./components/chatbot/ChatBot";
 
 export default function Home() {
   return (
-    <div>
+    <div className='flex justify-center items-center w-full h-screen bg-gray-200'>
+      <p className='font-sans text-6xl text-sky-500'>Personal Chatbot</p>
       <ChatBot />
     </div>
   )
